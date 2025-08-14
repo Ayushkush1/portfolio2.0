5597c724908a94d255ba314cf7503543c61d06c4
Webby Ayush <ayush2210019-d@akgec.ac.in>
2025-08-14 14:30:57 +0530

    chore: Update main.tsx for improved structure and organization

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import { HelmetProvider } from 'react-helmet-async'
 
 document.documentElement.classList.add('dark')
